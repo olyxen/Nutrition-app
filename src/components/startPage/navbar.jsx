da3ab525471cdb06795fbfb41ee022e1b89b5cb8
RiaPap <dai16121@uom.edu.gr>
2020-02-13 00:05:54 +0200

    Connect two modal each other.

--- src/components/startPage/navbar.jsx
+++ src/components/startPage/navbar.jsx
@@ -75,6 +75,13 @@ class Navbar extends Component {
                               <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""></input>
                               <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                               <p class="mt-5 mb-3 text-muted">© 2020</p>
+                              <div >
+                                You don't have an account? <a href="#"onClick={() => {
+                                                      this.onOpenModal();
+                                                      this.onCloseModalclose(); }}
+                                                      >Sign up
+                                                  </a>
+                            </div>
                           </form>
                         </div>
                 </div>
@@ -92,17 +99,19 @@ class Navbar extends Component {
                             <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
                             <label for="inputEmail" class="sr-only">Email address</label>
                             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""></input>
-                            <label for="inlineFormInput" class="sr-only" >Name</label>
-                            <input type="text" id="inlineFormInput" class="form-control" placeholder="Full Name"></input>
                             <label for="inlineFormInputGroup" class="sr-only" >Username</label>
                             <input type="text" id="inlineFormInputGroup" class="form-control" placeholder="Username"></input>
                             <label for="inputPassword" class="sr-only">Password</label>
                             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""></input>
                             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                             <p class="mt-5 mb-3 text-muted">© 2020</p>
-                            {/* <div >
-                                Have an account? <button onClick={this.onOpenModalLogin} >Log In</button>
-                            </div> */}
+                            <div >
+                                Have an account? <a href="#"onClick={() => {
+                                                      this.onOpenModalLogin();
+                                                      this.onCloseModal(); }}
+                                                      >Login
+                                                  </a>
+                            </div>
                         </form>
             
                          </div>
