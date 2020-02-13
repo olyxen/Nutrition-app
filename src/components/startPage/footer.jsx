9571fee51b588eeb669085366e59f76341549b30
RiaPap <dai16121@uom.edu.gr>
2020-02-14 00:16:53 +0200

    Some changes in appearance.

--- src/components/startPage/footer.jsx
+++ src/components/startPage/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
       <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <span class="text-muted">Place sticky footer content here. © 2020</span>
       </div>
   );
 }
--- src/components/startPage/mainbody.jsx
+++ src/components/startPage/mainbody.jsx
@@ -10,19 +10,24 @@ function MainBody() {
         <div class="container">
           <div class="row properties3">
             <div class="text-center col-lg-4">
-              <img src="/cover1.jpg" class="rounded"></img>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. </p>
+              <img src="/simple-planning.jpg" class="rounded"></img>
+              <h3>Simple planning</h3>
+              <p>Nutrition app can help you with managing your health conditions and taking better care of yourself. </p>
             </div>
             <div class="text-center col-lg-4">
-              <img src="/cover1.jpg" class="rounded"></img>             
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. </p>            
+              <img src="/easiest-food-tracking.jpg" class="rounded"></img>  
+              <h3>Easiest Food Tracking</h3>           
+              <p>Our food database is curated by trained staff to avoid the errors, missing nutrition data, and duplicates that plague other food diary services. </p>            
             </div>
             <div class="text-center col-lg-4">
-              <img src="/cover1.jpg" class="rounded"></img>             
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. </p>            
+              <img src="/advice-feedback.jpg" class="rounded"></img>  
+              <h3>Nutrition Analysis</h3>           
+              <p>In order to restrict caloric intake, but remain healthy, users of the diet must track their vitamin, mineral, and protein intakes with great care. </p>            
             </div>
           </div>
         </div>
+        <br/>
+        <br/>
         <hr/>
         <div class="container">
           <h2 class="text-center">About Us</h2>
