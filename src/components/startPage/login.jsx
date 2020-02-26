75e9c2903cb4337609c287291e2b0d2e253690a8
olyxen <dai16058@uom.edu.gr>
2020-02-26 21:19:01 +0200

    Activate login

--- src/components/startPage/login.jsx
+++ src/components/startPage/login.jsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import './login.css'
+import React, { Component} from 'react';
+import axios from "axios";
+import './login.css';
 
 
-function Login(){
+
+class Login extends Component {
+
+    constructor(props){
+        super(props);
+    }
+    state = {
+      username: null,
+      password: null,
+    };
+
+
+    login(){       
+        axios.post("http://localhost:8080/api/user/login/",{
+            username: this.state.username,
+            password: this.state.password
+        })
+        .then(res => {
+            if (res.data==='Logged in!'){
+                var token = res.headers['auth-token'];
+                localStorage.setItem("login", token);
+                window.open("/DashBoard", "_self"); //to open new page
+            }
+            else{
+                alert(res.data);
+            }
+        })
+    }
+
+render() { 
     return (
-     
-        <div class="text-center h-100 login">
-            <form class="form-signin ">
-                <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"></img>
-                <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""></input>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""></input>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">© 2020</p>
-            </form>
-        </div>
+        <>
+        <h2>Login and Get <span>Started</span></h2>
+
+                    
+        <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"></img>
+        <form name="form" onSubmit={(e) => {this.login(); e.preventDefault(); }}> 
+            <h1 class="h3 mb-3 font-weight-normal">Welcome to Nutri App</h1>
+            <label class="sr-only" >Username</label>
+            <input type="text" class="form-control" name="username" placeholder="Username" onChange={(event)=>{this.setState({username:event.target.value})}} required></input> 
+            <label class="sr-only">Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Password" onChange={(event)=>{this.setState({password:event.target.value})}} required></input>
+            <input class="btn btn-lg btn-primary btn-block" type="submit" value="Sign in"></input>
+        </form>
         
+        <p class="mt-5 mb-3 text-muted">© 2020</p>
+    </>
     )
 }
+}
+
 export default Login;
--- src/components/startPage/navbar.jsx
+++ src/components/startPage/navbar.jsx
@@ -3,6 +3,7 @@ import React, { Component} from 'react';
 import Modal from 'react-responsive-modal';
 import './login.css';
 import './frontPage.css';
+import Login from './login'
 
 
 
@@ -19,13 +20,11 @@ class Navbar extends Component {
  //for modals 
     constructor(props){
       super(props)
-
-      this.state = {
-        sign: false,
-        login: false,
-        final: false,
-
-      }
+    }
+    state = {
+      sign: false,
+      login: false,
+      final: false,
     }
 
     onOpenModal = () => {
@@ -99,16 +98,7 @@ class Navbar extends Component {
                   <button  id="btn" class="btn my-2 my-sm-0" onClick={this.onCloseModalclose}><img  id="closebutton" src="/x.png"></img></button>
                     <div class="text-center h-100 login">
                           <form class="form-signin " onSubmit={(e) => { e.preventDefault(); }}>
-                          <h2>Login and Get <span>Started</span></h2>
-                    
-                              <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"></img>
-                              <h1 class="h3 mb-3 font-weight-normal">Welcome to Nutri App</h1>
-                              <label for="inlineFormInputGroup" class="sr-only" >Username</label>
-                              <input type="text" id="inlineFormInputGroup" class="form-control" placeholder="Username"></input> 
-                              <label for="inputPassword" class="sr-only">Password</label>
-                              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""></input>
-                              <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                              <p class="mt-5 mb-3 text-muted">© 2020</p>
+                              <Login/>
                               <div >
                                 You don't have an account? <a href="#"onClick={() => {
                                                       this.onOpenModal();
@@ -118,6 +108,7 @@ class Navbar extends Component {
                             </div>
                           </form>
                         </div>
+                        
                 </div>
               </Modal>
 
