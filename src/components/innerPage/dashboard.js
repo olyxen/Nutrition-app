636fc9bf58c1dab8a354cfc205762534b9d3b3d6
olyxen <dai16058@uom.edu.gr>
2020-03-07 17:17:13 +0200

    Add actions for sidebar and menu

--- src/components/innerPage/dashboard.js
+++ src/components/innerPage/dashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Menu from './menu';
+import Sidebar from './sidebar';
+import './css/main.css'
+import './css/sidebar-themes.css'
+import jQuery from 'jquery'
 
 
 function DashBoard() {
@@ -7,11 +11,48 @@ function DashBoard() {
     <React.Fragment>
         <body class="d-flex flex-column h-100">
             <noscript>You need to enable JavaScript to run this app.</noscript>
-            {/* <header id="navbar" ><Navbar /></header> */}
-            <main role="main" class="flex-shrink-0"><Menu /></main>
+            <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+              <header id="navbar" ><Sidebar /></header>
+              <main role="main" class="flex-shrink-0"><Menu /></main>
+            </div>
         </body>
     </React.Fragment>
   );
 }
 
 export default DashBoard;
+
+
+jQuery(function ($) {
+
+  // Dropdown menu
+  $(".sidebar-dropdown > a").click(function () {
+      $(".sidebar-submenu").slideUp(200);
+      if ($(this).parent().hasClass("active")) {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this).parent().removeClass("active");
+      } else {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this).next(".sidebar-submenu").slideDown(200);
+          $(this).parent().addClass("active");
+      }
+
+  });
+
+  //toggle sidebar
+  $("#toggle-sidebar").click(function () {
+      $(".page-wrapper").toggleClass("toggled");
+  });
+
+  //toggle sidebar overlay
+  $("#overlay").click(function () {
+      $(".page-wrapper").toggleClass("toggled");
+  });
+
+  // toggle border radius
+  $("#toggle-border-radius").change(function (e) {
+      e.preventDefault();
+      $('.page-wrapper').toggleClass("border-radius-on");
+  });
+
+});
