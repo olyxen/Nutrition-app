a34c0168c75387b877d7deeb216ca75c692027a9
olyxen <dai16058@uom.edu.gr>
2020-03-07 17:16:47 +0200

    Add a provision sidebar and menu

--- src/components/innerPage/menu.jsx
+++ src/components/innerPage/menu.jsx
@@ -7,9 +7,25 @@ class Menu extends Component {
 
 render() { 
     return (
-        <>
-        <h2>LOGGED IN!!!</h2>
-    </>
+         <main class="page-content pt-2">
+         <div id="overlay" class="overlay"></div>
+         <div class="container-fluid p-5">
+             <div class="row">
+                 <div class="form-group col-md-12">
+                     <h2>Pro Sidebar</h2>
+                     <p>This is a responsive sidebar template with dropdown menu based on bootstrap framework.</p>
+
+                 </div>
+
+                 <div class="form-group col-md-12">
+                     <a id="toggle-sidebar" class="btn btn-secondary rounded-0" href="#">
+                         <span>Toggle Sidebar</span>
+                     </a>
+                 </div>
+             </div>
+             <hr/>
+         </div>
+     </main>
     )
 }
 }
